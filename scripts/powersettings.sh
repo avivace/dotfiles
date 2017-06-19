9241fd51d27b79068e56ee8d17ee77c94dbc5170
Antonio Vivace <puma95.95@gmail.com>
2017-06-19 10:16:43 +0200

    back to sid

--- scripts/powersettings.sh
+++ scripts/powersettings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # AC
 if [ $1 -eq 1 ]; then
-  compton -cCFb --backend glx --vsync opengl
+#  compton -cCFb --backend glx --vsync opengl
 # BAT ONLY
 elif [ $1 -eq 2 ]; then
-  killall compton
+#  killall compton
 fi
