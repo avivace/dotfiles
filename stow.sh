b3c57b726b935e5d009122c1946cecde595b8b89
Antonio Vivace <puma95.95@gmail.com>
2017-12-24 13:19:33 +0100

    Initial configuration deploy(stow) script

---
+++ stow.sh
@@ -0,0 +1,16 @@
+# Deploy the whole configuration ex-novo
+sudo stow apt
+sudo apt install
+sudo apt update
+sudo apt upgrade
+
+stow compton
+stow konsole
+stow plasma
+stow tmux
+stow X
+stow bash
+stow firefox
+stow i3
+stow sublime-text-3
+stow vim
