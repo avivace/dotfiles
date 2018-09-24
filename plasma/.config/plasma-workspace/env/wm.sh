ff830b764b1de86e92cb057f39ea8e3faab1e76d
Antonio Vivace <avivace4@gmail.com>
2018-09-24 08:59:36 +0200

    Run compton specifying configuration file

--- plasma/.config/plasma-workspace/env/wm.sh
+++ plasma/.config/plasma-workspace/env/wm.sh
@@ -1,8 +1,6 @@
 # Disable KWin and use i3gaps as WM
-export KDEWM=/usr/bin/i3
+export KDEWM=/usr/local/bin/i3
 
 # Compositor (Animations, Shadows, Transparency)
 # xcompmgr -C
-
-# Start compton as daemon (-b)
 compton -b --config ~/.config/compton/compton.conf
