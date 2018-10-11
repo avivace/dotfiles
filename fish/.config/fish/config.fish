86b910ec8acd8230b027216460a4bd0c8fb5cc3c
Antonio Vivace <avivace4@gmail.com>
2018-10-11 23:32:35 +0200

    Fish alias: replace cat with bat

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,6 @@
-alias update='sudo bash /home/avivace/update.sh'
+set PATH $HOME/.cargo/bin $PATH
+
+alias update='sudo bash ~/update.sh'
 alias win7='vboxmanage startvm "win7"'
 alias 3t='~/Downloads/3t/bin/robo3t'
-alias rpi='sshpass -praspberry ssh rpi'
+alias cat='bat'
